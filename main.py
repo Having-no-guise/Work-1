e453460e9d984dd10c2025cd408085d235af6143
Having-no-guise <eskelanir@mail.ru>
2022-03-04 16:16:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+
 def input_data():
     while True:
         print("Введите значение переменной:")
@@ -5,6 +8,30 @@ def input_data():
             return float(input())
         except ValueError as exc:
             print(f"Введеная строка не является числом. Попробуйте заново.\nПодробнее: {exc}")
+
+
+def menu():
+    count = 0
+    while True:
+        print("Чего пожелаете?\n0. Запустить функцию\n1. Просмотр истории\n2. Выход")
+        value = int(input_data())
+        if value == 0:
+            count += 1
+            main()
+            time.sleep(3)
+        elif value == 1:
+            if count == 0:
+                print("Стоит сначала запустить основную функцию")
+                time.sleep(3)
+            else:
+                history_file = open("Histroy.txt", 'r')
+                print(history_file.read())
+                history_file.close()
+                time.sleep(3)
+        elif value == 2:
+            print("Хорошего дня")
+            time.sleep(3)
+            exit(0)
 
 
 def print_output(output):
@@ -15,6 +42,7 @@ def history_save(output_data_1, output_data_2, output_data_3):
     history_file = open("Histroy.txt", 'w')  #в этом режиме он перезапишет весь файл
     history_file.write("Входные данные: \n" "X = %d\n" "N = %d\n" % (output_data_1, output_data_2))
     history_file.write("Результат (с точностью до тысячных):\n x^n = %.3f" % output_data_3)
+    history_file.close()
 
 
 def main():
@@ -26,4 +54,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    menu()
